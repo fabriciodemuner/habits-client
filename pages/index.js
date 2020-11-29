65c88f1998ff4d33f08734854024d3cf095904f4
fabricio.demuner <fabricio.demuner@gmail.com>
2020-11-29 22:56:13 +0100

    add index page

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,45 @@
-import React, { useState } from "react";
-const Home = () => {
-  return (
-    <>
-      <div>Home 2</div>
-      <Counter />
-    </>
+import React, { useEffect, useState } from "react";
+import AddHabit from "../src/AddHabit";
+import HabitsGrid from "../src/HabitsGrid";
+import Header from "../src/Header";
+import Axios from "axios";
+import { API_HOST } from "../src/constants";
+
+const get = Axios.get;
+function App() {
+  const [loading, setLoading] = useState(true);
+  const [rows, setRows] = useState(null);
+  const [add, setAdd] = useState(false);
+
+  useEffect(() => {
+    getHabits();
+  }, []);
+
+  async function getHabits() {
+    try {
+      const res = await get(`${API_HOST}/habit`);
+      setRows(res.data);
+      setLoading(false);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  const gridDiv = loading ? <p>Loading...</p> : <HabitsGrid rows={rows} onChange={getHabits} />;
+  const addDiv = add ? (
+    <AddHabit onChange={getHabits} setAdd={setAdd} />
+  ) : (
+    <button onClick={() => setAdd(true)}>Add Habit</button>
   );
-};
-
-const Counter = () => {
-  const [counter, setCounter] = useState(0);
 
   return (
-    <>
-      <button onClick={() => setCounter(counter - 1)}>Down</button>
-      <span> {counter} </span>
-      <button onClick={() => setCounter(counter + 1)}>Up</button>
-    </>
+    <div>
+      <Header />
+      {gridDiv}
+      <br />
+      {addDiv}
+    </div>
   );
-};
+}
 
-export default Home;
+export default App;

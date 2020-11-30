7a46aaa5f521789f0e01cf56bb64a336b67c5fb4
fabricio.demuner <fabricio.demuner@gmail.com>
2020-11-30 20:40:07 +0100

    hide api hostname

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,2 @@
 export const ___PROD___ = process.env.NODE_ENV === "production";
-export const API_HOST = ___PROD___
-  ? "https://habits-tracker-be.herokuapp.com"
-  : "http://localhost:4000";
+export const API_HOST = process.env.API_HOST || "http://localhost:4000";

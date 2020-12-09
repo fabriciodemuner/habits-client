4a2aafa898622d02d7d8e8ad11cab6d3e2c29d2f
fabricio.demuner <fabricio.demuner@gmail.com>
2020-12-09 17:32:51 +0100

    format ts files

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,2 @@
-export const ___PROD___ = process.env.NODE_ENV === "production";
-export const API_HOST =
-  process.env.NEXT_PUBLIC_API_HOST || "http://localhost:4000";
+export const ___PROD___ = process.env.NODE_ENV === 'production';
+export const API_HOST = process.env.NEXT_PUBLIC_API_HOST || 'http://localhost:4000';
